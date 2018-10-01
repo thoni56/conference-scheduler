8e3966f618aacb5487b9bc6639b622f73c5532a6
Thomas Nilefalk <thomas@nilefalk.se>
2018-10-01 09:20:54 +0200

    Attempt to calculate position from start time

--- import/ui/slot/slot.js
+++ import/ui/slot/slot.js
@@ -5,6 +5,13 @@ import './insertSlotForm.html';
 
 
 Template.slot.helpers({
-    top: "80",
-    height: "200"
+    top() {
+        const slotSize = 2;
+        const hm = this.start.split(":");
+        const s = (parseInt(hm[0])*60+parseInt(hm[1])-9*60)*slotSize;
+        return s;
+    },
+    height() {
+        return 200;
+    }
 });
