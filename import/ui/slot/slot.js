82148d99655465a0291b612847c90465e0b434d6
Thomas Nilefalk <thomas@nilefalk.se>
2018-10-02 22:37:48 +0200

    Move geometry calculation into a single helper

--- import/ui/slot/slot.js
+++ import/ui/slot/slot.js
@@ -11,13 +11,19 @@ const eventUnit = 15;   // minutes
 const timeLineStart = getScheduleTimestamp(startTime);
 
 Template.slot.helpers({
-    top() {
-        return (getScheduleTimestamp(this.start)-timeLineStart)/eventUnit*slotSize;
-    },
-    height() {
-        return this.length/eventUnit*slotSize;
+    geometry() {
+        const s = "style=\"top: "+top(this.start)+"px; height: "+height(this.length)+"px;\"";
+        return s;
     }
 });
+
+function top(start) {
+    return (getScheduleTimestamp(start)-timeLineStart)/eventUnit*slotSize;
+}
+
+function height(length) {
+    return length/eventUnit*slotSize;
+}
 
 function getScheduleTimestamp(time) {
     //accepts hh:mm format - convert hh:mm to "minute-stamp"
